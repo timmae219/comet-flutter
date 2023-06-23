c9a5057adef8f6fdf48d5fa3904db546a0ac33d9
Timucin Maurice Erdar <t.erdar@cupofsoftware.de>
2023-06-23 21:04:11 +0200

    Performs an initial project cleanup and includes assets and fonts

---
+++ lib/ui/main_screen/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Header Placeholder'),
+      ),
+      body: Container(),
+    );
+  }
+}
